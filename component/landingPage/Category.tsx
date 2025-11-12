cd1fb91f197c802dec6c34beb22b2e1bde33db60
maulanaibnusaban <ibnusaban67@gmail.com>
2025-11-12 18:41:49 +0700

    fix background

--- component/landingPage/Category.tsx
+++ component/landingPage/Category.tsx
@@ -3,14 +3,33 @@ import React from "react";
 
 function Category() {
   return (
-    <section className="relative h-[850px] w-full flex justify-center items-center bg-transparent">
-      <Image
-        src="/bg-category.svg"
-        alt="Category Background"
-        width={1921}
-        height={950}
-        className="w-full h-full absolute inset-0 -z-10 hidden xl:block"
-      />
+    <section className="relative h-[800px] w-full flex justify-center items-center bg-transparent mb-10">
+      <svg
+        className="w-full h-auto filter drop-shadow-[0_-10px_5px_rgba(0,0,0,0.07)] absolute top-0 left-0 right-0 -z-10 hidden lg:block"
+        width="2115"
+        height="227"
+        viewBox="0 0 2115 227"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2115 227H0V48.6768L28.6182 23.3652H415.516L472.752 73.9883H1167.29L1208.02 34.0732H1550.89L1589.96 0H2115V227Z"
+          fill="white"
+        />
+      </svg>
+      <svg
+        className="w-full h-auto filter drop-shadow-[0_12px_4px_rgba(0,0,0,0.1)] absolute bottom-0 left-0 right-0 -z-10 hidden lg:block"
+        width="2115"
+        height="211"
+        viewBox="0 0 2115 211"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2086.38 186.175L2115 159.281V0H0V211L525.035 211L564.11 174.797H906.979L947.705 132.388H1642.25L1699.48 186.175H2086.38Z"
+          fill="white"
+        />
+      </svg>
       <div className="flex flex-col justify-center items-center font-plus-jakarta-sans w-full px-10 gap-4">
         <h1 className="font-extrabold text-3xl lg:text-5xl 2xl:text-[53px]">KATEGORI LOMBA</h1>
         <p className="text-base lg:text-xl 2xl:text-2xl">kategori lomba DN Roboco 2026</p>
--- component/landingPage/Footer.tsx
+++ component/landingPage/Footer.tsx
@@ -3,14 +3,21 @@ import React from "react";
 
 function Footer() {
   return (
-    <section className="relative h-[250px] w-full flex justify-center items-end py-10">
-      <Image
-        src="/bg-footer.svg"
-        alt="Footer Background"
-        width={1080}
-        height={250}
-        className="w-full h-full absolute inset-0 -z-10 "
-      />
+    <section className="relative h-[150px] lg:h-[200px] w-full flex justify-center items-end py-10">
+      <svg
+        className="w-full h-auto filter drop-shadow-[0_-10px_5px_rgba(0,0,0,0.07)] absolute top-0 left-0 right-0 -z-10 "
+        width="2115"
+        height="140"
+        viewBox="0 0 2115 140"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M383.5 33H0V140H2115V33H1731.5L1700.5 2H1619L1588 33H1198L1165.5 0H938.5L906.5 33H527L496 2H414.5L383.5 33Z"
+          fill="white"
+        />
+      </svg>
+
       <Image src="/logo-title.svg" alt="Footer Image" width={340} height={163} className="w-[150px] lg:w-[250px] h-auto" />
     </section>
   );
