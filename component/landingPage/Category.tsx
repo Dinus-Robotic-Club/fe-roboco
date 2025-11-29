78201cbd5973568ea49a22524ea82a2ddc14ead2
maulanaibnusaban <ibnusaban67@gmail.com>
2025-11-30 02:09:23 +0700

    create : implement ux landing page

--- component/landingPage/Category.tsx
+++ component/landingPage/Category.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { categories } from "@/lib";
 import Image from "next/image";
-import Link from "next/link";
 import React, { useState } from "react";
+import { motion } from "motion/react";
 
 function Category() {
   const [active, setActive] = useState("soccerbot");
@@ -10,8 +10,22 @@ function Category() {
   return (
     <section className="relative w-full flex justify-center my-10 scroll-mt-24" id="category">
       <div className="flex flex-col font-plus-jakarta-sans w-full mx-3 sm:mx-10 lg:mx-20 gap-4 max-w-7xl">
-        <h1 className="font-bold text-4xl text-center md:text-start  lg:text-5xl 2xl:text-[53px]">Kategori Lomba</h1>
-        <p className="font-semibold text-base lg:text-xl text-center md:text-start">DN ROBOCO 2026</p>
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="font-bold text-4xl text-center md:text-start  lg:text-5xl 2xl:text-[53px]"
+        >
+          Kategori Lomba
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="font-semibold text-base lg:text-xl text-center md:text-start"
+        >
+          DN ROBOCO 2026
+        </motion.p>
 
         <div className="flex w-full font-plus-jakarta-sans mt-10 flex-wrap">
           <div className="flex bg-transparent max-w-5xl w-full">
@@ -32,44 +46,66 @@ function Category() {
           {/* Content Card */}
           <div className="bg-white w-full max-w-7xl rounded-b-3xl rounded-tr-3xl p-6 md:p-10 flex flex-col md:flex-row justify-between items-center gap-10 border-4 border-black">
             <div className="w-full md:w-1/2">
-              <h2 className="text-3xl font-bold mb-4 flex gap-3 items-center">
+              <motion.h2
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+                className="text-3xl font-bold mb-4 flex gap-3 items-center"
+              >
                 <Image src={selected.icon} alt="logo" height={35} width={35} />
                 {selected.title}
-              </h2>
+              </motion.h2>
 
-              <div className="space-y-4 text-lg leading-relaxed">
+              <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.6 }}
+                className="space-y-4 text-lg leading-relaxed"
+              >
                 {selected.description.map((p, i) => (
                   <p key={i}>{p}</p>
                 ))}
-              </div>
+              </motion.div>
 
               <div className="mt-8 text-center flex flex-col md:flex-row gap-4">
-                <Link
+                <motion.a
+                  initial={{ opacity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6, ease: "easeOut", delay: 0.6 }}
                   href="/register"
                   className="px-10 py-3 border-3 border-black bg-black text-[#FBFF00] font-semibold rounded-md shadow-md hover:bg-[#A8A8A8] transition flex items-center justify-center"
                 >
                   DAFTAR
-                </Link>
+                </motion.a>
 
-                <Link
+                <motion.a
+                  initial={{ opacity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.7, ease: "easeOut", delay: 0.7 }}
                   href={selected.guidebook}
                   download
                   className="px-10 py-3 border-3 border-black text-black rounded-lg font-semibold hover:bg-[#FAFAFA] transition flex justify-center items-center gap-4"
                 >
                   <Image src={selected.icon} alt="logo" width={20} height={20} className="" /> GUIDE BOOK
-                </Link>
+                </motion.a>
               </div>
             </div>
 
             {/* Image */}
             <div className="w-full md:w-1/2 flex justify-center">
-              <Image
-                src={selected.icon}
-                alt={selected.title}
-                width={450}
-                height={300}
-                className="w-[260px] md:w-[300px] h-auto drop-shadow-xl scale-x-[-1]"
-              />
+              <motion.div
+                initial={{ scale: 0 }}
+                whileInView={{ scale: 1 }}
+                transition={{ duration: 0.8, type: "spring", stiffness: 50, delay: 0.5 }}
+              >
+                <Image
+                  src={selected.icon}
+                  alt={selected.title}
+                  width={450}
+                  height={300}
+                  className="w-[260px] md:w-[300px] h-auto drop-shadow-xl scale-x-[-1]"
+                />
+              </motion.div>
             </div>
           </div>
         </div>
--- component/landingPage/Home.tsx
+++ component/landingPage/Home.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 function Home() {
   return (
@@ -15,21 +17,40 @@ function Home() {
       </div>
 
       <div className="z-10 w-full max-w-5xl flex flex-col items-center gap-6 sm:gap-8 lg:gap-10 px-4 sm:px-6 lg:px-10 py-8">
-        <Image
-          src="/logo-title.svg"
-          alt="logo-title"
-          width={488}
-          height={234}
-          className="w-full max-w-[340px] md:max-w-[400px] lg:max-w-[450px] 2xl:max-w-[550px] h-auto px-2"
-        />
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+          className="flex items-center justify-center"
+        >
+          <Image
+            src="/logo-title.svg"
+            alt="logo-title"
+            width={488}
+            height={234}
+            className="w-full max-w-[340px] md:max-w-[400px] lg:max-w-[450px] 2xl:max-w-[550px] h-auto px-2"
+          />
+        </motion.div>
 
-        <p className="text-base sm:text-lg md:text-xl 2xl:text-2xl text-center font-plus-jakarta-sans leading-relaxed max-w-2xl">
+        <motion.p
+          initial={{ y: -20, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.3 }}
+          className="text-base sm:text-lg md:text-xl 2xl:text-2xl text-center font-plus-jakarta-sans leading-relaxed max-w-2xl"
+        >
           Buktikan lah diri anda bahwa anda adalah seorang penggiat teknologi dengan mental yang kuat dalam kompetisi DN Roboco
           2026.
-        </p>
+        </motion.p>
 
         <div className="flex flex-col sm:flex-row gap-3 sm:gap-4 md:gap-5 w-full justify-center items-center max-w-md sm:max-w-none">
-          <button className="border-2 border-black bg-[#FBFF00] px-4 sm:px-5 md:px-7 lg:px-12 py-3 sm:py-4 text-sm sm:text-base md:text-lg w-full sm:w-fit font-plus-jakarta-sans flex items-center justify-center gap-2 sm:gap-2.5 transition-all cursor-pointer hover:scale-105">
+          <motion.a
+            href="/register"
+            initial={{ y: 30, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1 }}
+            viewport={{ once: true }}
+            className="border-2 border-black bg-[#FBFF00] px-4 sm:px-5 md:px-7 lg:px-12 py-3 sm:py-4 text-sm sm:text-base md:text-lg w-full sm:w-fit font-plus-jakarta-sans flex items-center justify-center gap-2 sm:gap-2.5 transition-all cursor-pointer hover:scale-105"
+          >
             <p>daftar sekarang</p>
             <Image
               src="/arrow-left.svg"
@@ -38,9 +59,16 @@ function Home() {
               height={20}
               className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
             />
-          </button>
+          </motion.a>
 
-          <button className="border-2 border-black  bg-[#FBFF00] px-4 sm:px-5 md:px-6 lg:px-12 py-3 sm:py-4 text-sm sm:text-base md:text-lg w-full sm:w-fit font-plus-jakarta-sans flex items-center justify-center gap-2 sm:gap-2.5 transition-all cursor-pointer hover:scale-105">
+          <motion.a
+            href="https://drive.google.com/drive/folders/1Q60AzUk0KvFz9bvyAFkqq4ezdzvIK4xn?usp=sharing"
+            initial={{ y: 30, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.2 }}
+            viewport={{ once: true }}
+            className="border-2 border-black  bg-[#FBFF00] px-4 sm:px-5 md:px-6 lg:px-12 py-3 sm:py-4 text-sm sm:text-base md:text-lg w-full sm:w-fit font-plus-jakarta-sans flex items-center justify-center gap-2 sm:gap-2.5 transition-all cursor-pointer hover:scale-105"
+          >
             <p>jurnal kompetisi</p>
             <Image
               src="/arrow-left.svg"
@@ -49,7 +77,7 @@ function Home() {
               height={20}
               className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
             />
-          </button>
+          </motion.a>
         </div>
       </div>
     </section>
