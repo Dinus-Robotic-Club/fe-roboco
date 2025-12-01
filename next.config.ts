2c6dd89371f5db94765b88436ed5d8691525a3fd
herehere <111202113618@mhs.dinus.ac.id>
2025-12-01 17:17:45 +0700

    baru bangun lagi

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,45 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
+   reactStrictMode: true,
 
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '3001',
-                pathname: '/uploads/**',
-            },
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '3001',
-                pathname: '/uploads/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'be-roboco-production.up.railway.app',
-                pathname: '/uploads/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'backend-dnroboco.dinusrobotic.org',
-                pathname: '/uploads/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'dnroboco01.dinusrobotic.org',
-                pathname: '/be/uploads/**',
-            },
-        ],
-    },
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'http',
+            hostname: 'localhost',
+            port: '3001',
+            pathname: '/uploads/**',
+         },
+         {
+            protocol: 'http',
+            hostname: '127.0.0.1',
+            port: '3001',
+            pathname: '/uploads/**',
+         },
+         {
+            protocol: 'https',
+            hostname: 'be-roboco-production.up.railway.app',
+            pathname: '/uploads/**',
+         },
+         {
+            protocol: 'https',
+            hostname: 'backend-dnroboco.dinusrobotic.org',
+            pathname: '/uploads/**',
+         },
+         {
+            protocol: 'https',
+            hostname: 'dnroboco01.dinusrobotic.org',
+            pathname: '/be/uploads/**',
+         },
+      ],
+   },
 
-    experimental: {
-        optimizeCss: true,
-        optimizePackageImports: ['lucide-react'],
-    },
+   experimental: {
+      optimizeCss: true,
+      optimizePackageImports: ['lucide-react'],
+   },
+   output: "standalone",
 }
 
 export default nextConfig
