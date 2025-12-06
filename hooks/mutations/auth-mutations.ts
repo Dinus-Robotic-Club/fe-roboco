e09d6018472188ad96b2f2a2739e2a8ceef43c2a
zappto <saptogusty@gmail.com>
2025-12-06 15:39:27 +0700

    feat: Implement password reset functionality with validation and API integration

---
+++ hooks/mutations/auth-mutations.ts
@@ -0,0 +1,32 @@
+import { forgotPassword, resetPassword } from '@/lib/api/forgot-reset'
+import { IApiResponse } from '@/lib/types/type'
+import { useMutation } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
+export const useForgotPassword = () => {
+    return useMutation<IApiResponse<unknown>, Error, { email: string }>({
+        mutationFn: ({ email }) => forgotPassword(email),
+
+        onSuccess: (data) => {
+            toast.success(data.message)
+        },
+        onError: (err) => {
+            console.log('Error send forgot password', err.message)
+            toast.error(err.message)
+        },
+    })
+}
+
+export const useResetPassword = () => {
+    return useMutation<IApiResponse<unknown>, Error, { token: string; newPassword: string }>({
+        mutationFn: ({ token, newPassword }) => resetPassword(token, newPassword),
+
+        onSuccess: (data) => {
+            toast.success(data.message)
+        },
+        onError: (err) => {
+            console.log('Error send forgot password', err.message)
+            toast.error(err.message)
+        },
+    })
+}
